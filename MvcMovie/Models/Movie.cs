e2b76759f38fc176005b419cfd7c7a11354624e3
Wdog-999 <wdog_999@yahoo.com>
2018-10-13 17:47:11 -0400

    Progress

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,19 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "The title must be between 3 and 60 characters.")]
+        [Required]
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The genre must be no longer than 30 characters.")]
         public string Genre { get; set; }
+        [Range(1, 100, ErrorMessage = "Price must be between 1 and 100.")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
